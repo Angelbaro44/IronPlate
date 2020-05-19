d50c7efa83082dd57d2f0c4967dd0b46dc4c309f
Angel Baro <angelbaro44@gmail.com>
2020-05-19 15:16:20 -0400

    test

---
+++ frontend/build/precache-manifest.88edce09a017442c90d8c6d97c43a967.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f600ac136a4a2408ff0a4147f895e9ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "815f5c19bc4ee8bebc6b",
+    "url": "/static/css/main.daac1ec2.chunk.css"
+  },
+  {
+    "revision": "48c765507f7f2c11ed52",
+    "url": "/static/js/2.ab4c9669.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ab4c9669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "815f5c19bc4ee8bebc6b",
+    "url": "/static/js/main.78c80193.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  }
+]);
